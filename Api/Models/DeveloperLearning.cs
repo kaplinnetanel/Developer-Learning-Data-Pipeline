6888ab9fb3be21507af97e08946cb39fe4bd4d41
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-09-03 15:46:20 +0300

    finish Day

---
+++ Api/Models/DeveloperLearning.cs
@@ -0,0 +1,26 @@
+﻿namespace Api.Models;
+
+public class DeveloperLearning
+{
+    public int ResponseId { get; set; }
+    public string? Age { get; set; }
+    public int? YearsCode { get; set; }
+    public string? DevType { get; set; }
+    public string? LearnCodeChoose { get; set; }
+
+    public List<string>? LearnCode { get; set; }
+    public string? LearnCodeAI { get; set; }
+    public List<string>? AILearnHow { get; set; }
+
+    public string? AISelect { get; set; }
+    public string? AIAcc { get; set; }
+    public string? AISent { get; set; }
+
+    public string? ExperienceLevel { get; set; }
+
+    public bool UsesDocumentation { get; set; }
+    public bool UsesAIForLearning { get; set; }
+    public bool UsesStackOverflow { get; set; }
+}
+
+
--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Consumer.Models;
+using MongoDB.Driver;
+using System.Text.Json;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "csharp-consumer",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+var mongoClient = new MongoClient("mongodb://localhost:27017");
+var database = mongoClient.GetDatabase("DeveloperLearningDb");
+var collection = database.GetCollection<DeveloperLearning>("Developers");
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+
+consumer.Subscribe("processed-survey-topic");
+
+try
+{
+    while (true)
+    {
+        var result = consumer.Consume();
+
+        var developer = JsonSerializer.Deserialize<DeveloperLearning>(
+            result.Message.Value
+        );
+
+        if (developer != null)
+        {
+            await collection.InsertOneAsync(developer);
+
+            Console.WriteLine(
+                $"Saved ResponseId: {developer.ResponseId}"
+            );
+        }
+    }
+}
+catch (OperationCanceledException)
+{
+    consumer.Close();
+}
